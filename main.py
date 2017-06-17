f12d445bde3d9a8d3a92d14c43b611592b66d5b8
Aaron Pham <aaronp9485@gmail.com>
2017-06-17 16:08:18 -0700

    form displays again after submission

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method= 'POST'>
             <label for="rotateby">Rotate by: </label>
             <input id= "rotateby" type='text' name="rot"/><br>
-            <textarea name="text" rows="4" cols="50"></textarea><br>
+            <textarea name="text" rows="4" cols="50">{0}</textarea><br>
             <input type="submit" value="Submit Query"/>
         </form>
     </body>
@@ -37,14 +37,16 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format(" ")
 
 @app.route("/", methods=['POST'])
 def encrypt():
     num = int(request.form["rot"])
     words = request.form["text"]
     new_text = rotate_string(words, num)
-    return "<h1>" + new_text + "</h1>"
+    return form.format(new_text)
+
+    # "<h1>" + new_text + "</h1>"
 
 
 app.run()
